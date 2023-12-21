6fedf974199fd85737db4f761bd1cb6ac72bbd7d
yamasora1006k <yamaguchisora1006@gmail.com>
2023-12-21 15:23:43 +0900

    うううう

--- login_icon.js
+++ login_icon.js
@@ -2,35 +2,38 @@
 
 // login_icon.js
 document.addEventListener("DOMContentLoaded", function () {
-  // アイコンプレビュー画像の要素を取得
-  const iconPreview = document.getElementById("iconPreview");
-  // ファイル入力要素を取得
   const iconInput = document.getElementById("iconInput");
-  // ログインボタンの要素を取得
   const loginButton = document.getElementById("loginButton");
+  const iconPreview = document.getElementById("iconPreview");
+  const iconImage = document.getElementById("iconImage");
+  const errorMessage = document.getElementById("error-message");
 
-  // アイコンプレビューがクリックされたときにファイル入力を開くイベントリスナー
   iconPreview.addEventListener("click", function () {
     iconInput.click();
   });
 
-  // ファイルが選択されたときにプレビューを更新するイベントリスナー
   iconInput.addEventListener("change", function (event) {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onload = function (e) {
-        // プレビュー画像を表示
         iconPreview.style.backgroundImage = `url(${e.target.result})`;
+        iconImage.style.display = "none"; // 新しい画像が選択されたら元の画像を非表示にする
       };
       reader.readAsDataURL(file);
     }
   });
 
-  // ログインボタンがクリックされたときのイベント
   loginButton.addEventListener("click", function () {
-    // ここにログイン処理を実装します
-    // 例えば、サーバーへのリクエストを送信するコードなど
-    alert("ログイン処理を実装してください。");
+    if (!iconInput.value) {
+      // 画像が選択されていない場合はエラーメッセージを表示
+      errorMessage.textContent = "アイコンが選択されていません。";
+    } else {
+      // 画像が選択されている場合は、ここでログイン処理を実装
+      // 例: サーバーに画像をアップロードするコード
+      errorMessage.textContent = "";
+      // ログイン処理が成功したと仮定して次のページへ遷移
+      // window.location.href = '次のページのURL';
+    }
   });
 });
